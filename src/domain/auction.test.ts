b939c77077a24de2c57b346627dcc9602ebb7921
kawamura-lvgs <yuki.kawamura@leverages.jp>
2024-01-16 13:44:10 +0900

    Add: auction.ts

--- src/domain/auction.test.ts
+++ src/domain/auction.test.ts
@@ -1,27 +1,9 @@
 import { describe } from "node:test";
+import { Auction } from "./auction";
 
 afterEach(() => {
   jest.useRealTimers();
 });
-
-class Auction {
-  start(): Auction {
-    return new Auction(this.id, this.startDateTime, this.endDateTime, true);
-  }
-  constructor(
-    readonly id: string,
-    readonly startDateTime: Date,
-    readonly endDateTime: Date,
-    readonly isStarted: boolean = false
-  ) {
-    if (startDateTime < new Date()) {
-      throw new Error("開始時刻が過去です");
-    }
-    if (endDateTime < startDateTime) {
-      throw new Error("終了時刻が開始時刻より過去です");
-    }
-  }
-}
 
 describe("Auction", () => {
   test("初期化できる（作成できる）", () => {
