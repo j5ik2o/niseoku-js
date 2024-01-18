715be1bb1f5aa57227ca974b5975cbb12957a4f9
maehara-lvgs <sotaro.maehara@leverages.jp>
2024-01-18 17:55:14 +0900

    Fix: 同じ価格で２回入札できない

--- src/domain/auction.ts
+++ src/domain/auction.ts
@@ -3,10 +3,9 @@ export class Auction {
     readonly id: string,
     readonly startDateTime: Date,
     readonly endDateTime: Date,
-    readonly startBidPrice: number,
+    readonly bidPrice: number,
     readonly isStarted: boolean = false,
     readonly now: Date = new Date(),
-    readonly bidPrice: number = 0,
   ) {
     if (startDateTime < now) {
       throw new Error("開始時刻が過去です");
@@ -23,9 +22,8 @@ export class Auction {
     startBidPrice: number,
     isStarted: boolean = false,
     now: Date = new Date(),
-    bidPrice: number = 0,
   ): Auction {
-    return new Auction(id, startDateTime, endDateTime, startBidPrice, isStarted, now, bidPrice);
+    return new Auction(id, startDateTime, endDateTime, startBidPrice, isStarted, now);
   }
 
   start(now: Date = new Date()): Auction {
@@ -35,16 +33,14 @@ export class Auction {
     return new Auction(this.id, this.startDateTime, this.endDateTime, 100, true);
   }
 
-  bid(_price: number): Auction {
+  bid(price: number): Auction {
     if (!this.isStarted) {
       throw new Error("オークションが開始していません");
     }
-    if (_price <= this.bidPrice) {
+    if (price <= this.bidPrice) {
       throw new Error("現在の最高額より低い金額で入札することはできません");
     }
-    if (this.startBidPrice < _price) {
-      return new Auction(this.id, this.startDateTime, this.endDateTime, this.startBidPrice, this.isStarted, this.now, _price);
-    }
-    return this
+    
+    return new Auction(this.id, this.startDateTime, this.endDateTime, price, this.isStarted, this.now);
   }
 }
